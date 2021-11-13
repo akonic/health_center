71f5e1b837954063d1abb911018a74732b4d2c03
akonic <ankitkachhi.ak47@gmail.com>
2021-11-14 00:18:27 +0530

    new database added

--- hc_admin_create.php
+++ hc_admin_create.php
@@ -14,29 +14,17 @@ session_start();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"
         integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous" />
-    <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
     <!--link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"-->
-    <title>Hello, world!</title>
+    <title>Admin HC</title>
 </head>
 
 <body>
 
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white" style="box-shadow: 0px 5px 15px  rgb(166, 166, 166);">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">HealthISM</a>
-            <!--button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Features</a>
-              <a class="nav-link" href="#">Pricing</a>
-              <a class="nav-link disabled">Disabled</a>
-            </div>
-            <!-->
-        </div>
+        <div class="container-fluid"><a class="navbar-brand" href="http://localhost/mis/"><img src = "images/iitismlogo.png" style = "width: 50px; height: 50px;">   HealthISM</a></div>
 
         <div class="nav-item dropdown">
             <a class="nav-link" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown"
@@ -44,93 +32,61 @@ session_start();
                 <i class="bi bi-person-circle" style="color: grey; font-size: x-large;"></i>
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a class="dropdown-item" href="#">Home</a></li>
-                <li><a class="dropdown-item" href="#">Logout</a></li>
+                <li><a class="dropdown-item" href="http://localhost/mis/">Home</a></li>
+                <li><a class="dropdown-item" href="http://localhost/mis/index.php/login/logout">Logout</a></li>
             </ul>
         </div>
         </div>
     </nav>
 
-    <!--Old Cards>
-        <div class="row" style="margin: 50px">
-            <div class="col-sm-12 col-md-4">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title" style="text-align: center; font-size:x-large;"> My Treatments</h5>
-                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                  <a href="#" class="btn buttons card-icons"><i class="fas fa-notes-medical"></i></a>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-12 col-md-4">
-                <div class="card">
-                  <div class="card-body">
-                    <h5 class="card-title" style="text-align: center; font-size:x-large;"> Tests Available</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn buttons card-icons"><i class="fas fa-flask"></i></a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-12 col-md-4">
-                <div class="card">
-                  <div class="card-body">
-                    <h5 class="card-title" style="text-align: center; font-size:x-large;"> Enquiry</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn buttons card-icons"><i class="fas fa-phone-alt"></i></a>
-                  </div>
-                </div>
-              </div>
-          </div>
-          <!-->
-
-
-    <!--New Cards-->
+    
+    <!--Cards-->
     <div class="container-fluid mb-5">
         <div class="text-center mt-5">
             <h1>Our Services</h1>
         </div>
         <div class="row">
-            <div class="col-md-4" style="border:1px solid black;">
+            <div class="col-md-3 col-sm-6">
                 <a href="http://localhost/health_center/hc_admin_create.php">
                     <div class="box">
-                        <div class="our-services settings">
-                            <div class="icon"> <img src="images/medfile.png" style="width: 70px; height: 70px;"> </div>
+                        <div class="our-services create-record">
+                            <div class="icon"> <img src="images/create-record.png" style="width: 70px; height: 70px;"> </div>
                             <h4>Create Record</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <p>Enter a new consultation record</p>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-md-4" style="border:1px solid black;">
+            <div class="col-md-3 col-sm-6">
                 <a href="http://localhost/health_center/hc_admin_treatments.php">
                     <div class="box">
-                        <div class="our-services settings">
+                        <div class="our-services treatments">
                             <div class="icon"> <img src="images/medfile.png" style="width: 70px; height: 70px;"> </div>
                             <h4>Treatments</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <p>View and update all the treatment records</p>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-md-4" style="border:1px solid black;">
+            <div class="col-md-3 col-sm-6">
                 <a href="http://localhost/health_center/hc_admin_lab.php">
                     <div class="box">
-                        <div class="our-services speedup">
-                            <div class="icon"> <img src="images/microsope.png" style="width: 70px; height: 70px;">
+                        <div class="our-services labs">
+                            <div class="icon"> <img src="images/lab.png" style="width: 70px; height: 70px;">
                             </div>
                             <h4>Lab Records</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <p>Manage pathology labs</p>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-md-4" style="border:1px solid black;">
+            <div class="col-md-3 col-sm-6">
                 <a href="http://localhost/health_center/hc_admin_doctor.php">
                     <div class="box">
-                        <div class="our-services privacy">
-                            <div class="icon"> <img src="images/enquiry.png" style="width: 70px; height: 70px;"> </div>
+                        <div class="our-services doctors">
+                            <div class="icon"> <img src="images/doctor.png" style="width: 70px; height: 70px;"> </div>
                             <h4>Doctors</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>
+                            <p>Manage doctors and staff's database</p>
                         </div>
                     </div>
                 </a>
@@ -142,6 +98,8 @@ session_start();
         <form action="" method="POST">
             <label for="lname">Patient Name :</label>
             <input type="text" id="pnaem" name="pname"><br>
+            <label for="lname">Patient ID :</label>
+            <input type="text" id="pnaem" name="pid"><br>
             <label for="lname">Doctor Name :</label>
             <input type="text" id="dname" name="dname"><br>
             <label for="lname">Test Prescribed</label>
@@ -170,9 +128,9 @@ session_start();
    $doctor=$_POST['dname'];
    $test=$_POST['tests'];
    $date=date("d-m-Y");
-
-   $insert = " insert into hc_registration(patient_name,doctor_name,test_prescribed,date)
-    values ('$name','$doctor','$test','$date')";
+   $ids=$_POST['pid'];
+   $insert = " insert into hc_registration(patient_name,patient_id,doctor_name,test_prescribed,date)
+    values ('$name','$ids','$doctor','$test','$date')";
 
     $res = mysqli_query($con,$insert);
      if($res)
--- hc_admin_doctor.php
+++ hc_admin_doctor.php
@@ -17,9 +17,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"
         integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous" />
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
     <!--link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet"-->
-    <title>Hello, world!</title>
+    <title>Admin HC</title>
 </head>
 
 <body>
@@ -27,18 +27,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white" style="box-shadow: 0px 5px 15px  rgb(166, 166, 166);">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">HealthISM</a>
-            <!--button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Features</a>
-              <a class="nav-link" href="#">Pricing</a>
-              <a class="nav-link disabled">Disabled</a>
-            </div>
-            <!-->
+            <a class="navbar-brand" href="http://localhost/mis/"><img src = "images/iitismlogo.png" style = "width: 50px; height: 50px;">   HealthISM</a>
         </div>
 
         <div class="nav-item dropdown">
@@ -47,93 +36,61 @@
                 <i class="bi bi-person-circle" style="color: grey; font-size: x-large;"></i>
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a class="dropdown-item" href="#">Home</a></li>
-                <li><a class="dropdown-item" href="#">Logout</a></li>
+                <li><a class="dropdown-item" href="http://localhost/mis/">Home</a></li>
+                <li><a class="dropdown-item" href="http://localhost/mis/index.php/login/logout">Logout</a></li>
             </ul>
         </div>
         </div>
     </nav>
 
-    <!--Old Cards>
-        <div class="row" style="margin: 50px">
-            <div class="col-sm-12 col-md-4">
-              <div class="card">
-                <div class="card-body">
-                  <h5 class="card-title" style="text-align: center; font-size:x-large;"> My Treatments</h5>
-                  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                  <a href="#" class="btn buttons card-icons"><i class="fas fa-notes-medical"></i></a>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-12 col-md-4">
-                <div class="card">
-                  <div class="card-body">
-                    <h5 class="card-title" style="text-align: center; font-size:x-large;"> Tests Available</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn buttons card-icons"><i class="fas fa-flask"></i></a>
-                  </div>
-                </div>
-              </div>
-              <div class="col-sm-12 col-md-4">
-                <div class="card">
-                  <div class="card-body">
-                    <h5 class="card-title" style="text-align: center; font-size:x-large;"> Enquiry</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn buttons card-icons"><i class="fas fa-phone-alt"></i></a>
-                  </div>
-                </div>
-              </div>
-          </div>
-          <!-->
-
-
-    <!--New Cards-->
+    
+    <!--Cards-->
     <div class="container-fluid mb-5">
         <div class="text-center mt-5">
             <h1>Our Services</h1>
         </div>
         <div class="row">
-            <div class="col-md-4" style="border:1px solid black;">
+            <div class="col-md-3 col-sm-6">
                 <a href="http://localhost/health_center/hc_admin_create.php">
                     <div class="box">
-                        <div class="our-services settings">
-                            <div class="icon"> <img src="images/medfile.png" style="width: 70px; height: 70px;"> </div>
+                        <div class="our-services create-record">
+                            <div class="icon"> <img src="images/create-record.png" style="width: 70px; height: 70px;"> </div>
                             <h4>Create Record</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <p>Enter a new consultation record</p>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-md-4" style="border:1px solid black;">
+            <div class="col-md-3 col-sm-6">
                 <a href="http://localhost/health_center/hc_admin_treatments.php">
                     <div class="box">
-                        <div class="our-services settings">
+                        <div class="our-services treatments">
                             <div class="icon"> <img src="images/medfile.png" style="width: 70px; height: 70px;"> </div>
                             <h4>Treatments</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <p>View and update all the treatment records</p>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-md-4" style="border:1px solid black;">
+            <div class="col-md-3 col-sm-6">
                 <a href="http://localhost/health_center/hc_admin_lab.php">
                     <div class="box">
-                        <div class="our-services speedup">
-                            <div class="icon"> <img src="images/microsope.png" style="width: 70px; height: 70px;">
+                        <div class="our-services labs">
+                            <div class="icon"> <img src="images/lab.png" style="width: 70px; height: 70px;">
                             </div>
                             <h4>Lab Records</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                            <p>Manage pathology labs</p>
                         </div>
                     </div>
                 </a>
             </div>
-            <div class="col-md-4" style="border:1px solid black;">
+            <div class="col-md-3 col-sm-6">
                 <a href="http://localhost/health_center/hc_admin_doctor.php">
                     <div class="box">
-                        <div class="our-services privacy">
-                            <div class="icon"> <img src="images/enquiry.png" style="width: 70px; height: 70px;"> </div>
+                        <div class="our-services doctors">
+                            <div class="icon"> <img src="images/doctor.png" style="width: 70px; height: 70px;"> </div>
                             <h4>Doctors</h4>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>
+                            <p>Manage doctors and staff's database</p>
                         </div>
                     </div>
                 </a>
@@ -141,10 +98,10 @@
         </div>
     </div>
     <div class="container">
-        <a href="http://localhost/health_center/hc_admin_doctor_record.php">
-            <div>Add doctors Record</div>
+        <a href="http://localhost/health_center/hc_admin_doctor_record.php"><i class="fa fa-plus" aria-hidden="true"></i>
+           <b>Add Doctors Record</b>
         </a>
-    </div>
+    </div><br>
     <!--Enquiry Table-->
     <div class="container" id="33" style="display: block;">
         <table class="table">
